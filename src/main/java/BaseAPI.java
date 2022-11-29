dde27888f03d9845d08edec978c0ec22fe3da3ff
psasha007 <psasha007@yandex.ru>
2022-11-29 15:32:36 +0300

    Добавил класс LoginUserTest проверки Авторизации пользователя

---
+++ src/main/java/BaseAPI.java
@@ -0,0 +1,13 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import static constants.Constants.BASE_URL;
+
+public class BaseAPI {
+    protected RequestSpecification requestSpecification(){
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri(BASE_URL)
+                .build();
+    }
+}
